9d0da8451f8a2d87a2876d34ec0dead8c4e3bed8
katiepalumbo <palumbo.katie@gmail.com>
2022-02-15 12:42:45 +0100

    inizio ad integrare js

--- main.js
+++ main.js
@@ -3,6 +3,57 @@ const app = new Vue (
         el: '#app',
         data: {
             newMessage: '',
+            active: 0,
+            contacts: [
+                {
+                    nome: 'Marianna',
+                    avatar: '_2',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            text: 'Hai risolto il problema di javascript?',
+                            status: 'sent'
+                        }
+                    ]
+                },
+                {
+                    nome: 'Alice',
+                    avatar: '_io',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            text: 'Hai risolto il problema di javascript?',
+                            status: 'sent'
+                        }
+                    ]
+                },
+                {
+                    nome: 'Angela',
+                    avatar: '_6',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            text: 'Hai risolto il problema di javascript?',
+                            status: 'sent'
+                        }
+                    ]
+                },
+                {
+                    nome: 'Boolean Class #53',
+                    avatar: '_8',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            text: 'Hai risolto il problema di javascript?',
+                            status: 'sent'
+                        }
+                    ]
+                }
+            ]
 
         }
     }
